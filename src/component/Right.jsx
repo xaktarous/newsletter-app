d9df0143791a133d57faa922fc28d0791578f4d8
delali abdallah <83099194+xaktarous@users.noreply.github.com>
2024-05-12 21:16:06 +0100

    Delete src directory

--- src/component/Right.jsx
+++
@@ -1,9 +0,0 @@
-import desk from "../assets/illustration-sign-up-desktop.svg"
-const Right = () => {
-  return (
-    <div className="right desk">
-      <img src={desk}/>
-      </div>
-  )
-}
-export default Right
